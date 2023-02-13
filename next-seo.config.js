019535a1bacc3e180adf4ac47897d5ec52583805
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2023-02-14 11:29:29 +1300

    Updated SEO

--- next-seo.config.js
+++ next-seo.config.js
@@ -2,15 +2,15 @@
 const defaultSEOConfig = {
   defaultTitle: "QR Generator",
   description: "A simple web app to generate the QR Code.",
-  canonical: "https://qr.yehezgun.com",
+  canonical: "https://qr.tewahi.com",
   openGraph: {
-    url: "https://qr.yehezgun.com",
+    url: "https://qr.tewahi.com",
     title: "QR Generator",
     description: "A simple web app to generate the QR Code.",
     type: "website",
     images: [
       {
-        url: "https://og-v2.yehezgun.com/api/og?title=QR%20Generator&desc=A%20simple%20web%20app%20to%20generate%20the%20QR%20Code.&imgUrl=https://cdn-icons-png.flaticon.com/512/1233/1233055.png?w=740&t=st=1669899478~exp=1669900078~hmac=cdb7088b7f105d5383eb605a29732548b0798f2895f3f4fe06afbb0ed5a35f17&siteName=qr.yehezgun.com",
+        url: "https://cdn.tewahi.me/qr.png",
         alt: "qr-code",
         width: 1200,
         height: 630,
